38f9ffbfccd0e8f1badb536bb69915e69936ea61
e1211913 <e1211913-sanuk_sangrou>
2024-02-02 10:13:42 +0000

    latest

--- bin/main.ml
+++ bin/main.ml
@@ -1,10 +1,12 @@
 module E1 = Cs3110.Example1
 
 let () =
-  print_endline "Running below examples...";
-  let examples = [
-    Int.to_string E1.number;
-    Int.to_string (E1.frec 10);
-    Int.to_string (E1.fact 10)
-  ] in
+  print_endline "Run examples from the lectures..";
+  let examples =
+    [ Int.to_string E1.number
+    ; Int.to_string @@ E1.frec 10
+    ; Int.to_string @@ E1.fact 10
+    ; Int.to_string @@ E1.len [ 1; 2; 3; 4; 5 ]
+    ]
+  in
   List.iter print_endline examples
--- lib/example1.ml
+++ lib/example1.ml
@@ -5,3 +5,57 @@ let rec frec x =
   if x < 2 then 2 else inner + frec (x - 1)
 
 let rec fact x = if x <= 2 then 2 else x * fact (x - 1)
+
+(* matching *)
+type person =
+  { name : string
+  ; password : int
+  }
+
+let match_record r =
+  match r with
+  | { name = "darchitect"; _ } -> "you're in!"
+  | { name; password } -> Int.to_string password ^ name
+
+let match_tuple t =
+  match t with
+  | 1, 2 -> 2
+  | a, _ -> a
+
+let match_sugar = function
+  | "" -> 128
+  | "something" -> 129
+  | _ -> 666
+
+let rec len = function
+  | [] -> 0
+  | _ :: t -> 1 + len t
+
+let rec rotate lst = function
+  | 0 -> lst
+  | k -> rotate List.(tl lst @ [ hd lst ]) (k - 1)
+
+(* variants *)
+
+type color =
+  | Red
+  | Green
+  | Blue
+
+type point = int * int
+
+type shape =
+  | Circle of
+      { center : point
+      ; radius : float
+      }
+  | Rectangle of
+      { lower_left : point
+      ; upper_right : point
+      }
+
+let get_center = function
+  | Circle { center; _ } -> center
+  | Rectangle { lower_left; upper_right } ->
+    (* below is meaningless *)
+    fst lower_left + fst upper_right, snd lower_left + snd upper_right
