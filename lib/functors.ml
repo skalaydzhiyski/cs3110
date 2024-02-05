60134c08ab93cf7635732a9f9c3cbe0acd740c16
skalaydzhiyski <spas.kalaydzhiyski@gmail.com>
2024-02-05 19:12:12 +0000

    add example of functors

--- lib/functors.ml
+++ lib/functors.ml
@@ -1,1 +1,21 @@
-(** TODO: continue here when you're coming back to OCaml *)
+module type X = sig
+  val x : int
+  val modify : int -> int
+  val to_overwrite : int -> int
+end
+
+module A : X = struct
+  let x = 128
+  let modify x = x + 1
+  let to_overwrite x = x
+end
+
+module A' (M : X) = struct
+  let x = M.x + 1
+  let modify x = x |> M.modify |> M.modify |> ( * ) 2
+  let to_overwrite x y z = x + y + z
+end
+
+module B = A' (A)
+
+let res = B.(modify x)
