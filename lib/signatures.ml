a70d4c6e54e7a159892e1ef86d7583b06301f33c
darchitect <spas.kalaydzhiyski@gmail.com>
2024-02-05 15:27:24 +0000

    add examples of signatures and abstract types

--- lib/signatures.ml
+++ lib/signatures.ml
@@ -1,1 +1,30 @@
 (* TODO: continue here when you're coming back to 3110 *)
+
+module type Interface = sig
+  type t
+
+  val create : unit -> t
+  val f : t -> string
+end
+
+module MyIntImpl : Interface = struct
+  type t = int
+
+  let create () = 128
+  let f x = "<" ^ Int.to_string x ^ ">"
+end
+
+module MyStringImpl : Interface = struct
+  type t = string
+
+  let create () = "something"
+  let f s = "<" ^ s ^ ">"
+end
+
+module MyInt : Interface = MyIntImpl
+module MyString : Interface = MyStringImpl
+
+let int1 = MyInt.create ()
+let string1 = MyString.create ()
+let intres = MyInt.(create () |> f)
+let stringres = MyString.(create () |> f)
