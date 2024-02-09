67dd84fd515ff825c7a241ca2af7dc0e191710cf
skalaydzhiyski <spas.kalaydzhiyski@gmail.com>
2024-02-09 18:19:19 +0000

    add iterative functions for insert and print

--- lib/mutable_linked_list.ml
+++ lib/mutable_linked_list.ml
@@ -20,9 +20,29 @@ let rec print_list = function
     Printf.printf "%d -> " head.value;
     print_list head.next
 
-let rec insert_end v n =
-  match n.next with
+let rec insert_end v head =
+  match head.next with
   | None ->
-    let n' = make_node v in
-    n.next <- Some n'
+    let new_node = make_node v in
+    head.next <- Some new_node
   | Some next -> insert_end v next
+
+let print_list_iter head =
+  let current = ref head in
+  while !current <> None do
+    match !current with
+    | None -> print_endline "X"
+    | Some n ->
+      print_endline @@ Int.to_string n.value;
+      current := (Option.get !current).next
+  done
+
+let insert_end_iter v head =
+  let current = ref (Some head) in
+  while !current <> None do
+    let n = Option.get !current in
+    match n.next with
+    | None -> n.next <- Some (make_node v)
+    | Some n' -> current := n'.next
+  done;
+  head
