74a88cfa9e00aed9d4113112cbd329a318d721d4
darchitect <spas.kalaydzhiyski@gmail.com>
2024-02-09 11:23:12 +0000

    add example of aliases

--- lib/refs.ml
+++ lib/refs.ml
@@ -8,5 +8,10 @@ let test_ref = ref 128
 let test () =
   let x = ref 128 in
   x += 128;
-  let res = !x * 2 in
-  Format.sprintf "res = %d" res
+  print_endline @@ Format.sprintf "x = %d" !x;
+  print_endline "make alias";
+  let y = x in
+  print_endline "update alias";
+  (* this modifies the underlying reference contents *)
+  y := 1;
+  print_endline @@ Format.sprintf "x = %d" !x
